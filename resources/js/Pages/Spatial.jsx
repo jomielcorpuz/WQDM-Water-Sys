f7ca5570fc937f26b618b0f9786cd2aba2bfbcb2
Miel Corpuz Banogbanog <93494269+mielbanog@users.noreply.github.com>
2025-02-10 18:41:42 +0800

    Added a table for displaying details of water quality for public viewers

--- resources/js/Pages/Spatial.jsx
+++ resources/js/Pages/Spatial.jsx
@@ -4,6 +4,19 @@ import SelectInput from "@/Components/SelectInput";
 import FrontLayout from "@/Layouts/FrontLayout";
 import { RadialChart } from "@/Components/publicspatialcomp/radialchart";
 import { Card } from "@/Components/ui/card";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "@/Components/ui/dropdown-menu";
+import { Button } from "@/Components/ui/button";
+import { Check } from "lucide-react"
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { ChevronDownIcon } from "@heroicons/react/16/solid";
 
 
 export default function Spatial() {
@@ -15,6 +28,8 @@ export default function Spatial() {
   const [map, setMap] = useState(null);
   const markers = useRef([]);
   const [filteredData, setFilteredData] = useState([]);
+  const [selectedValue, setSelectedValue] = useState("all");
+
 
   // Fetch water quality data
   useEffect(() => {
@@ -33,6 +48,13 @@ export default function Spatial() {
 
     fetchData();
   }, []);
+
+  // Set filteredData to all waterQualityData initially when data is fetched
+  useEffect(() => {
+    if (!loading) {
+      setFilteredData(waterQualityData);  // Set filtered data to all data when loading is done
+    }
+  }, [loading, waterQualityData]);  // Trigger this useEffect after the data is loaded
 
   // Initialize map
 
@@ -68,7 +90,7 @@ export default function Spatial() {
 
   // Add markers to the map
   useEffect(() => {
-    if (!map) return;
+    if (!map || !filteredData.length) return;
     // Clear existing markers
     markers.current.forEach(marker => marker.setMap(null));
     markers.current = [];
@@ -88,7 +110,7 @@ export default function Spatial() {
         <div style="text-align: left; padding: 0; margin: 0;">
         <div style="text-align: left;">
         <h1 style="font-size: 18px; font-weight: bold; margin-bottom: 8px;">${data.name}</h1>
-        <p><strong>pH Level:</strong> ${data.ph_level}</p>
+        <p><strong>PH Level:</strong> ${data.ph_level}</p>
         <p><strong>Water Condition:</strong> ${data.status}</p>
         <p><strong>Latitude:</strong> ${data.latitude}</p>
         <p><strong>Longitude:</strong> ${data.longitude}</p>
@@ -119,78 +141,131 @@ export default function Spatial() {
 
   //GOOGLE MAP API KEY = AIzaSyDf4-glzKgqzlRro_kOgsljGjPF9z1Y-_o
 
-  // Handle SelectInput change
-  const handleSelectChange = (e) => {
-    const selectedValue = e.target.value;
+  // Handle select change for filtering data
+  const handleSelectChange = (value) => {
+    setSelectedValue(value)
     const filtered = waterQualityData.filter(site =>
-      site.status === selectedValue || selectedValue === "all");
-    setFilteredData(filtered);
+      site.status === value || value === "all"
+    )
+    setFilteredData(filtered)
+  }
+
+  const defaultMarkerData = {
+    name: "No Marker Selected",
+    address: "No address available",
   };
+
+  // Determine if we have selected marker data, otherwise use default values
+  const currentMarkerData = selectedMarkerData || defaultMarkerData;
+  const currentAddress = selectedAddress || "Fetching address...";
 
   return (
     <div>
       <FrontLayout>
-        <div className="grid grid-cols-2 gap-6 mt-6">
+        <div className="grid lg:grid-cols-2 sm:grid-cols-1 gap-6 p-6">
 
           <Card>
             {/* Map section */}
-            <div className="bg-white border rounded overflow-hidden shadow-sm sm:rounded-lg">
-              <div className="p-4 bg-white dark:bg-gray-800">
-                <div className="flex justify-between items-center mb-2">
-                  <h1 className="text-xl font-bold">Water Sites Map</h1>
-                  <div className="flex items-center">
-                    <span className="text-sm font-semibold mr-2">View by:</span>
-                    <SelectInput onChange={handleSelectChange}>
-                      <option value="all">All</option>
-                      <option value="Potable">Potable</option>
-                      <option value="Non-potable">Non-potable</option>
-                    </SelectInput>
-                  </div>
+            <div className="p-4">
+              <div className="flex justify-between items-center mb-2">
+                <h1 className="text-xl font-semi-bold text-gray-800 ">Water Sites Map</h1>
+                <div className="flex items-center">
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button variant="outline">Filter
+                        <ChevronDownIcon />
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-56">
+                      <DropdownMenuLabel>Choose Water Quality</DropdownMenuLabel>
+                      <div className="space-y-2 p-2">
+                        <DropdownMenuItem
+                          onClick={() => handleSelectChange("all")}
+                          className={selectedValue === "all" ? "bg-blue-100" : ""}
+                        >
+                          {selectedValue === "all" && <Check className="mr-2" />}
+                          All
+                        </DropdownMenuItem>
+                        <DropdownMenuItem
+                          onClick={() => handleSelectChange("Potable")}
+                          className={selectedValue === "Potable" ? "bg-blue-100" : ""}
+                        >
+                          {selectedValue === "Potable" && <Check className="mr-2" />}
+                          Potable
+                        </DropdownMenuItem>
+                        <DropdownMenuItem
+                          onClick={() => handleSelectChange("Non-potable")}
+                          className={selectedValue === "Non-potable" ? "bg-blue-100" : ""}
+                        >
+                          {selectedValue === "Non-potable" && <Check className="mr-2" />}
+                          Non-potable
+                        </DropdownMenuItem>
+                      </div>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
                 </div>
-                <hr className="my-2 border-t-2 border-gray-300" />
-                {loading ? (
-                  <p>Loading...</p>
-                ) : (
-                  <div className="flex flex-row">
-                    {/* Map */}
-                    <div ref={mapRef} className="w-full h-96 border rounded mt-2 mb-2"></div>
-                  </div>
-                )}
               </div>
+              <hr className="my-2 border-t-2 border-gray-300" />
+              {loading ? (
+                <p>Loading...</p>
+              ) : (
+                <div className="flex flex-row">
+                  {/* Map */}
+                  <div ref={mapRef} className="w-full h-[450px] border rounded mt-2 mb-2"></div>
+                </div>
+              )}
             </div>
           </Card>
           {/* Details Section */}
-          <div className="bg-white p-4 overflow-hidden shadow-sm sm:rounded-lg border rounded">
-            <h1 className="text-xl font-bold mb-2 mt-2">Sites Reading</h1>
-            <hr className="my-2 mt-4 mb-3 border-t-2 border-gray-300" />
-            <div className="flex flex-col justify-center items-center">
-
-              <div className="w-full p-4 border rounded shadow bg-gray-200">
-                {selectedMarkerData ? (
-                  <div>
-                    <h3 className="font-bold text-lg mb-2">{selectedMarkerData.name}</h3>
-                    <p>
-                      <strong>pH Level:</strong> {selectedMarkerData.ph_level}
-                    </p>
-                    <p>
-                      <strong>Water Condition:</strong> {selectedMarkerData.status}
-                    </p>
-                    <p>
-                      <strong>Latitude:</strong> {selectedMarkerData.latitude}
-                    </p>
-                    <p>
-                      <strong>Longitude:</strong> {selectedMarkerData.longitude}
-                    </p>
-                    <p>
-                      <strong>Address:</strong> {selectedAddress || "Fetching address..."}
-                    </p>
-                  </div>
-                ) : (
-                  <p className="text-gray-500">Click a marker to see details.</p>
-                )}
-              </div>
-            </div>
-          </div>
+          <Card className="p-4">
+            <Table>
+              <TableCaption>Details of the selected marker details.</TableCaption>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Field</TableHead>
+                  <TableHead>Value</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <TableRow>
+                  <TableCell className="font-medium">Name</TableCell>
+                  <TableCell>{currentMarkerData.name}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="font-medium">pH Level</TableCell>
+                  <TableCell>{currentMarkerData.ph_level}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="font-medium">Water Condition</TableCell>
+                  <TableCell>{currentMarkerData.status}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="font-medium">Total Dissolve Solids</TableCell>
+                  <TableCell>{currentMarkerData.total_dissolved_solids} NTU</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="font-medium">Salinity</TableCell>
+                  <TableCell>{currentMarkerData.salinity}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="font-medium">Nitrate</TableCell>
+                  <TableCell>{currentMarkerData.nitrate}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="font-medium">Latitude</TableCell>
+                  <TableCell>{currentMarkerData.latitude}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="font-medium">Longitude</TableCell>
+                  <TableCell>{currentMarkerData.longitude}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="font-medium">Address</TableCell>
+                  <TableCell>{currentAddress}</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </Card>
         </div>
         <div className="py-10">
           <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
