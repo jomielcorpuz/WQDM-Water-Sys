6372c5be1fa914004b142fc6bf22254174501172
Miel Corpuz Banogbanog <93494269+mielbanog@users.noreply.github.com>
2025-02-13 18:01:51 +0800

    fix responsiveness

--- resources/js/Layouts/NewFooter.jsx
+++ resources/js/Layouts/NewFooter.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-8xl">
         <div className="grid grid-cols-2 md:col-span-3 lg:grid-cols-6 gap-y-16 gap-x-12">
           <div className="col-span-2 md:col-span-3 lg:col-span-2 lg:pr-8">
-            <img className="w-auto h-13" src="/images/dssc_header.png" alt="" />
+            <img className="w-auto h-13 md:h-12 sm:h-10" src="/images/dssc_header.png" alt="" />
 
             <p className="text-base leading-relaxed text-gray-600 mt-7">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
 
